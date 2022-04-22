002aa0bcfbdd2e65a92049d5e4f742303169135e
Гость <Guest@iMac-Mac005-9.local>
2022-04-22 09:50:20 +0900

    mediaresourses

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -9,23 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
-    @IBOutlet weak var ButtonRavno: UIButton!
-    @IBOutlet weak var ButtonZero: UIButton!
-    @IBOutlet weak var ButtonPlus: UIButton!
-    @IBOutlet weak var ButtonThree: UIButton!
-    @IBOutlet weak var ButtonTwo: UIButton!
-    @IBOutlet weak var ButtonONe: UIButton!
-    @IBOutlet weak var Minus: UIButton!
-    @IBOutlet weak var ButtonSix: UIButton!
-    @IBOutlet weak var ButtonFive: UIButton!
-    @IBOutlet weak var ButtonFour: UIButton!
-    @IBOutlet weak var umnozhenie: UIButton!
-    @IBOutlet weak var ButtonNine: UIButton!
-    @IBOutlet weak var ButtonEight: UIButton!
-    @IBOutlet weak var ButtonSeven: UIButton!
-    @IBOutlet weak var Delenie: UIButton!
-    @IBOutlet weak var ButtonAC: UIButton!
-    
+    @IBOutlet weak var buttonChangeResultButton: UIStepper!
+    @IBOutlet weak var buttons: UIStackView!
     var numberOne = ""
     var numberTwo = ""
     var operand = ""
@@ -33,22 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ButtonAC.layer.cornerRadius = 36
-        ButtonSix.layer.cornerRadius = 36
-        ButtonTwo.layer.cornerRadius = 36
-        ButtonFive.layer.cornerRadius = 36
-        ButtonFour.layer.cornerRadius = 36
-        ButtonNine.layer.cornerRadius = 36
-        ButtonPlus.layer.cornerRadius = 36
-        ButtonZero.layer.cornerRadius = 36
-        ButtonRavno.layer.cornerRadius = 36
-        ButtonEight .layer.cornerRadius = 36
-        ButtonSeven.layer.cornerRadius = 36
-        ButtonThree .layer.cornerRadius = 36
-        ButtonONe.layer.cornerRadius = 36
-        Delenie.layer.cornerRadius = 36
-        umnozhenie.layer.cornerRadius = 36
-        Minus.layer.cornerRadius = 36
+        buttons.layer.cornerRadius = 36
     }
 
     @IBAction func InputNumber(_ sender: UIButton) {
@@ -100,6 +70,11 @@ class ViewController: UIViewController {
             resultLabel.text = String(result)
         }
     }
+    @IBAction func hiddenButtons(_ sender: Any) {
+        // resultLabel.isHidden = !resultLabel.isHidden
+        buttons.isHidden = !buttons.isHidden
+    }
     
+
 }
 
